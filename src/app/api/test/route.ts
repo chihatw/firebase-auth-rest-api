6d8800cb7cdecf771220e7ce0c27c2d68ce275ce
chihatw <chiha.tw@gmail.com>
2023-07-22 18:33:39 +0900

    debug

--- src/app/api/test/route.ts
+++
@@ -1,5 +0,0 @@
-import { NextApiRequest } from 'next';
-
-export async function GET(Request: NextApiRequest) {
-  return new Response('This is a new API route');
-}
--- src/app/api/username/[user]/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest } from 'next';
-
-export async function GET(
-  request: NextApiRequest,
-  { params }: { params: { user: string } }
-) {
-  const user = params.user;
-  return new Response(`Welcome to my Next application, user: ${user}`);
-}
